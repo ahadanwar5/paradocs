58f3d2ad88254d3c2fb567ca5014e79ce440096b
Ahad Anwar <mailahadanwar@gmail.com>
2022-12-30 05:29:32 +0000

    commit message

--- project_frontend/src/components/Newsletter.jsx
+++ project_frontend/src/components/Newsletter.jsx
@@ -8,7 +8,7 @@ const Newsletter = () => {
     <div className='new-container'>
         <div className="new-row">
             <div className="new-col">
-                <h2 className="new-title">Newsletter</h2>
+                <h2 className="new-title">Newsletters</h2>
                 <p className="new-desc">Get timely updates from your favorite products.</p>
                 <div className="input-container">
                     <input type="text" placeholder='Your E-mail' />
